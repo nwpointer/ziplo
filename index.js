68afee3cc4938de5bad17fcc319dbd21064afa8f
nathanPointer <nwpointer@gmail.com>
2016-09-11 19:50:04 -0700

    dir

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var PORT = 80;
 var express = require('express');
 var app = express();
 
-app.use(express.static('static'));
+app.use('/', express.static(__dirname + '/static'));
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT);
