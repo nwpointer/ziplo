c4b5d76d67c6cb12c573a1d621267841fa71ba2c
nathanPointer <nwpointer@gmail.com>
2016-09-22 08:35:19 -0700

    copy and image

---
+++ README.txt
@@ -0,0 +1,11 @@
+finish framer prototype
+
+apply new copy
+
+add illustrations
+
+review landing pages for completeness
+
+figure out email/fb signups strategy
+
+build functional prototype
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var PORT = 80;
+var config = require('./config.json');
 var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname + '/static'));
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT);
+app.listen(config.PORT, function () {
+  console.log('Example app listening on port ' + config.PORT);
 });
